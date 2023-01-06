1608096b75d6bc4b3356a8bd2b6c21f7061b362a
Anand Dewangan <118416821+AnandDewangan@users.noreply.github.com>
2023-01-06 20:51:42 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -3,3 +3,14 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+class Shape{
+    drawshape(){
+    };
+    calculateArea(){};
+    color(){};
+}
+
+class Circle extends Shape{
+    super();
+}
